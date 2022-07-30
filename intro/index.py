1d51e527df6c1f1bf108938f4789e9d14b101357
Javad Pourmostafa Roshan Sharami <pourmost@mint.uvt.nl>
2022-07-30 18:43:52 +0200

    Add PyTorch's optimizer, partial derivation, etc

--- intro/index.py
+++ intro/index.py
@@ -1,10 +1,17 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
+import torch
+import torch.optim as optim
+torch.manual_seed(0)
+from torchviz import make_dot
+
+
 np.random.seed(42)
 
-x = np.random.rand(100, 1)
+# python and numpy
 
+x = np.random.rand(100, 1)
 y = 1 + 2 * x
 
 idx = np.arange(100)
@@ -43,6 +50,51 @@ for e in range(n_epochs):
 
 print(a, b)
 
+# sklearn
+
 linr = LinearRegression()
 linr.fit(x_train, y_train)
 print(linr.intercept_, linr.coef_[0])
+
+# PyTorch
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+x_train_tensor = torch.from_numpy(x_train).float().to(device)
+y_train_tensor = torch.from_numpy(y_train).float().to(device)
+
+print(type(x_train), type(x_train_tensor), x_train_tensor.type())
+
+a = torch.randn(1, requires_grad=True, dtype=torch.float, device=device)
+b = torch.randn(1, requires_grad=True, dtype=torch.float, device=device)
+
+print(a, b)
+
+# Defining a SGD optimizer to update the parameters
+optimizer = optim.SGD([a, b], lr=lr)
+
+for epoch in range(n_epochs):
+
+    yhat = a + b * x_train_tensor
+    error = y_train_tensor - yhat
+    loss = (error ** 2).mean()
+
+    loss.backward()
+    print(a.grad)
+    print(b.grad)
+
+    # with torch.no_grad():
+    #     a -= lr * a.grad
+    #     b -= lr * b.grad
+    
+    # a.grad.zero_()
+    # b.grad.zero_()
+
+    optimizer.step()
+
+    optimizer.zero_grad()
+
+
+    
+print(a, b)
+# visualization
+make_dot(error)
